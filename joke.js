8e7233139dabb5fa8cfed800dec113bd5b491381
ParthibanRajalingam <parthiban.rajalingam@gmail.com>
2019-01-27 08:39:18 +0530

    visitors count and Ip tracking functionality added

--- joke.js
+++ joke.js
@@ -12,14 +12,46 @@ const url = 'mongodb://parth:shark13@ds213705.mlab.com:13705/jokes';
 // Database Name
 const dbName = 'jokes';
 
+//logging
+const fs = require('fs');
+const util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
  // parse application/json
 app.use(bodyParser.json());
+
+
+
+
+
 app.use(function(req,res,next){
-      res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    //Get user's IP
+    try{
+    	 const ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+         req.connection.remoteAddress || 
+         req.socket.remoteAddress || 
+         req.connection.socket.remoteAddress;
+         console.log('IP'+ip);
+         //Update visitor count
+         updateVisiotorsCount();
+    }
+    catch(error){
+    	console.log('Error '+error);
+    }
+
     next();
-})
+});
+
+
 
 app.get('/joke', (req, res) => {
 
@@ -31,13 +63,13 @@ MongoClient.connect(url, function(err, db) {
 
   dbo.collection("sequence").find({}).toArray( function(err, result) {
     if (err) throw err;
-    console.log(result[0].jokes);
+    //console.log(result[0].jokes);
     var max= result[0].jokes;
     var randomJokeId = Math.floor(Math.random() * max) + min ;
 
     dbo.collection("joke").find({"id": randomJokeId}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result);
+   // console.log(result);
       res.send(result);
     db.close();
   });
@@ -60,13 +92,13 @@ MongoClient.connect(url, function(err, db) {
 
   dbo.collection("sequence").find({}).toArray( function(err, result) {
     if (err) throw err;
-    console.log(result[0].jokes);
+   // console.log(result[0].jokes);
     var max= result[0].jokes;
     var randomJokeId = Math.floor(Math.random() * max) + min ;
 
     dbo.collection("joke").find({"id": randomJokeId}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result);
+    //console.log(result);
       res.send(result);
     db.close();
   });
@@ -81,7 +113,7 @@ MongoClient.connect(url, function(err, db) {
 
 
 app.post('/joke', (req, res) =>{
-console.log(req.body);
+//console.log(req.body);
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -92,19 +124,19 @@ MongoClient.connect(url, function(err, db) {
 //Getting id
     dbo.collection("sequence").find({}).toArray( function(err, result) {
     if (err) throw err;
-    console.log(result[0].jokes);
+   // console.log(result[0].jokes);
     id = result[0].jokes;
     myobj["id"] = id;
     myobj["approved"] ="false";
     myobj["likes"] = 0;
     dbo.collection("joke").insertOne(myobj, function(err, result) {
     if (err) throw err;
-    console.log("1 document inserted");
+   // console.log("1 document inserted");
 
 
     dbo.collection("sequence").updateOne({"jokes":id}, {$set:{"jokes":++id}}, function(err, result) {
     if (err) throw err;
-    console.log("1 document updated");
+  //  console.log("1 document updated");
     db.close();
     res.send(result);
   });
@@ -126,7 +158,7 @@ MongoClient.connect(url, function(err, db) {
 
     dbo.collection("joke").updateOne({"id":req.body.id}, {$set:{"likes":req.body.likes}}, function(err, result) {
     if (err) throw err;
-    console.log("1 document updated");
+   // console.log("1 document updated");
     db.close();
     res.send(result);
   });
@@ -136,6 +168,66 @@ MongoClient.connect(url, function(err, db) {
 
  
 
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function updateVisiotorsCount() {
+
+	MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("jokes");
+  var myobj = {'date':getDate(),'visitorCount':1};
+  var count;
+
+//Getting id
+    dbo.collection("visitorCount").find({'date':myobj.date}).toArray( function(err, visitorResult) {
+    if (err) throw err;
+    if(visitorResult.length){
+
+    count = visitorResult[0].visitorCount + 1;
+   // console.log('---count---'+count);
+    dbo.collection("visitorCount").updateOne({'date':String(myobj.date)}, {$set:{"visitorCount": count}}, function(err, result) {
+    if (err) throw err;
+    //console.log("1 document updated in visitorCount");
+      });
+
+    }else{
+    dbo.collection("visitorCount").insertOne(myobj, function(err, result) {
+    if (err) throw err;
+   // console.log("1 document inserted visitorCount");
+
+    db.close();
+    
+  });
+    }
+
+
+  });
+
+
+
+});
+  
+}
+
+//ddmmyyyy
+function getDate(){
+
+	var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+today =  dd+  mm + yyyy;
+return today;
+
+}
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
